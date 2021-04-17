0f6c6499c319fce036861a48e54c76471120dd33
sjh0522 <74394708+sjh0522@users.noreply.github.com>
2021-04-17 22:23:06 +0900

    Add files via upload

---
+++ CompositePattern/Waitress.java
@@ -0,0 +1,16 @@
+package Composite;
+
+import java.util.Iterator;
+
+public class Waitress {
+	
+	MenuComponent allMenus;
+	 
+	public Waitress(MenuComponent allMenus) {
+		this.allMenus = allMenus;
+	} 
+	public void printMenu() {
+		allMenus.print();
+	}
+	
+}
